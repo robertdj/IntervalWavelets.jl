f48431061a0f6634769f1772e0474570419fd486
Robert DJ <git@dahl-jacobsen.dk>
2016-03-30 21:06:19 +0200

    Parsing shorthand wavelet names

--- src/misc.jl
+++ src/misc.jl
@@ -1,11 +1,11 @@
 @doc """
 	eigval1(A::Matrix) -> Vector
 
-Returns a unit eigenvector of `A` for the eigenvalue 1, if this eigenspace is 1D.
+Returns a unit eigenvector of `A` for the eigenvalue 1 if this eigenspace is 1D.
 Otherwise, return an error.
 """->
 function eigval1(A::Matrix)
-	evals = eigvals(A)
+	evals, evecs = eig(A)
 
 	# Find index of eigenvalue 1
 	isevals1 = map( x->isapprox(x,1.0), evals )
@@ -17,10 +17,38 @@ function eigval1(A::Matrix)
 		error("The eigenspace of 1 is more than 1D")
 	end
 
-	# Eigenvector of 1
-	D = evals[eval1_index[]]*eye(A)
-	evec = nullspace( A - D )
+	return evecs[:,eval1_index[]]
+end
 
-	return evec
+
+@doc """
+	waveparse(wavename::AbstractString) -> Vector
+
+Return `wavename`'s filter.
+
+	waveparse(wavename::AbstractString, true) -> ScalingFilters
+
+Return `wavename`'s internal and boundary filters.
+
+Currently, only Daubechies wavelets are supported and they are denoted 
+
+- `"Haar"`.
+- `"dbN"` where `N` is an integer between 1 and 8.
+"""->
+function waveparse(wavename::AbstractString, boundary::Bool=false)
+	# TODO: Enforce Daubechies only
+	lowername = lowercase( wavename )
+
+	# The Haar wavelet does not have boundary issues
+	if lowername == "haar" || lowername == "db1"
+		boundary = false
+	end
+
+	if !boundary
+		C = wavelet( WT.eval(parse(lowername)) ).qmf
+	else
+		vm = WT.vanishingmoments( WT.eval(parse(lowername)) )
+		C = scalingfilters(vm)
+	end
 end
 
