a47aaa8eb7e6acc6f737ebac2b00313cb75ac718
Robert DJ <git@dahl-jacobsen.dk>
2019-11-25 20:49:13 +0100

    Add filters for Daubechies scaling functions

---
+++ src/Filters.jl
@@ -0,0 +1,82 @@
+# ------------------------------------------------------------
+# Functions and types for general filters
+
+struct Filter
+	coefficients::OffsetArrays.OffsetVector{Float64, Vector{Float64}}
+end
+
+coefficients(h::Filter) = h.coefficients
+support(h::Filter) = h |> coefficients |> LinearIndices
+Base.length(h::Filter) = h |> support |> length
+
+function Base.show(io::IO, h::Filter)
+	show(io, coefficients(h))
+end
+
+function Base.getindex(h::Filter, idx::Int)
+	if checkbounds(Bool, coefficients(h), idx)
+		return coefficients(h)[idx]
+	else
+		return 0.0
+	end
+end
+
+
+# ------------------------------------------------------------
+# Functions and types for interacting with interior filters
+
+struct InteriorFilter
+	vanishing_moment::Int64
+	filter::Filter
+
+	function InteriorFilter(p, filter)
+		if p >= 0
+			new(p, filter)
+		else
+			throw(AssertionError("Not a valid interior filter"))
+		end
+	end
+end
+
+filter(h::InteriorFilter) = h.filter
+vanishing_moment(h::InteriorFilter) = h.vanishing_moment
+coefficients(h::InteriorFilter) = h |> filter |> coefficients
+support(h::InteriorFilter) = h |> filter |> support
+Base.length(h::InteriorFilter) = h |> filter |> length
+
+Base.getindex(h::InteriorFilter, idx) = filter(h)[idx]
+
+function Base.show(io::IO, h::InteriorFilter)
+	S = support(h)
+	println(io, "Filter for Daubechies ", vanishing_moment(h), 
+			" scaling function on [", S[1], ", ", S[end], "]:")
+	show(io, h.filter)
+end
+
+
+"""
+	interior_filter(p::Int[, phase=:symmlet])
+
+Filter for Daubechies scaling function with `p` vanishing moments and `phase` being `:symmlet` or 
+`:min` (minimum phase).
+"""
+function interior_filter(p::Integer, phase::Symbol=:symmlet)
+	if p < 1 
+		throw(DomainError())
+	end
+
+    return InteriorFilter(p, Filter(interior_filter(p, Val{phase})))
+end
+
+function interior_filter(p::Integer, ::Type{Val{:symmlet}})
+    if !(2 <= p <= 8)
+        throw(DomainError())
+    end
+
+    OffsetArrays.OffsetVector(INTERIOR_COEFFICIENTS[p], -p+1:p)
+end
+
+function interior_filter(p::Integer, ::Type{Val{:min}})
+    OffsetArrays.OffsetVector(Wavelets.wavelet(Wavelets.WT.Daubechies{p}()).qmf, 0:2*p-1)
+end
+
--- src/IntervalWavelets.jl
+++ src/IntervalWavelets.jl
@@ -1,12 +1,26 @@
 module IntervalWavelets
 
+import OffsetArrays
+import Wavelets
+
 export 
     DyadicRational,
     DyadicRationalVector,
+    Filter,
+    InteriorFilter,
 
+    coefficients,
+    interior_filter,
     numerator,
-    scale
+    scale,
+    support
+
+const sqrt2 = sqrt(2)
 
 include("DyadicRationals.jl")
+include("Filters.jl")
+include("Filters/Interior.jl")
+include("Filters/Left.jl")
+include("Filters/Right.jl")
 
 end # module
