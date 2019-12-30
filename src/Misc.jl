beb10a56268559a10a49c953a15d19a99031694d
Robert DJ <git@dahl-jacobsen.dk>
2019-12-30 21:07:26 +0100

    Fix type instability

--- src/Misc.jl
+++ src/Misc.jl
@@ -4,23 +4,17 @@
 If `A` has 1 as an eigenvalue of multiplicity 1, the corresponding eigenvector is returned.
 Otherwise, an error is thrown.
 """
-function eigval1(A::AbstractMatrix)
+function eigval1(A::AbstractMatrix)::Vector{Float64}
     eigen_values = LinearAlgebra.eigvals(A)
 
-    # TODO: Does this ensure that output type is a real matrix?
     if !isreal(eigen_values)
         throw(DomainError(eigen_values, "Eigen values should be real"))
     end
 
     eigen_value_one = findall(eigen_values .≈ 1)
 
-    # TODO: Are these good exceptions?
-    if isempty(eigen_value_one)
-        throw(InvalidStateException)
-    end
-
-    if length(eigen_value_one) > 1
-        throw(InvalidStateException)
+    if length(eigen_value_one) != 1
+        throw(DomainError(eigen_value_one, "The eigenvalue 1 should have multiplicity 1"))
     end
 
     return LinearAlgebra.eigvecs(A)[:, eigen_value_one[1]]
