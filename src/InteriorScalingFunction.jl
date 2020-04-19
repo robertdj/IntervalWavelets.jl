34826651831ba04468277fb6c31729c3cc5560a3
Robert DJ <git@dahl-jacobsen.dk>
2020-04-19 20:13:51 +0200

    Create matrix more compactly

--- src/InteriorScalingFunction.jl
+++ src/InteriorScalingFunction.jl
@@ -162,19 +162,10 @@ Return the "dyadic dilation matrix" from an interior filter used to compute the 
 function in the integers of its support.
 """
 function dyadic_dilation_matrix(h::InteriorFilter)
-    h_length = length(h)
-    sz = h_length - 2
-
-    H = Matrix{Float64}(undef, sz, sz)
-    filter_coefficients = coefficients(h)
-
+    sz = length(h) - 2
     left_support = support_boundaries(h)[1]
-    for col in 1:sz, row in 1:sz
-        h_idx = 2*col - row + left_support
-        H[col, row] = sqrt2 * h[h_idx]
-    end
 
-    return H
+    [sqrt2 * h[2*i - j + left_support] for i = 1:sz, j = 1:sz]
 end
 
 
